c2146fdfd603afe005055dad2d81badb034b4b37
chuck-gcc <3wll@proton.me>
2025-08-18 12:10:22 -0400

    default_push

--- sorting_machine/sorting_machine.py
+++ sorting_machine/sorting_machine.py
@@ -21,14 +21,40 @@ def get_obj_json(filepath):
             print("Loading object error")
             return(None)
 
+# Building the path liste of file to process
+def get_path_lst(dirpath):
+    path_liste  = []
+    # get the folder list to process
+    folders_list = os.listdir(dirpath)
+    if(len(folders_list) <= 0):
+        print("No folder to process ")
+        #if the folder is empty, delete the folder
+        os.rmdir(dirpath)
+        return(None)
+    #for each folder, get the list of file, build path and push them in path list arr
+    # if the folder is empty, delete de folder
+    for folder in folders_list:
+        files_list = os.listdir(dirpath+'/'+ folder)
+        if(len(files_list) <= 0):
+            os.rmdir(dirpath+'/'+ folder)
+            continue
+        print("files list" , files_list)
+        for file in files_list:
+            path = dirpath+'/'+ folder + '/' + file
+            path_liste.append(path)
+    assert(len(path_liste) == 4)
+    return (path_liste)
 
+    
 
-def main():
+def main(departement):
 
     #get file list to check
-    dirpath = "/home/cc/Documents/data_worker/ftdb/data/74/2025-08-18T13:46:44.631Z"
-    files_list = get_files_lst(dirpath)
-    if(files_list == None or len(files_list) <= 0):
+    dirpath = '/home/cc/Documents/data_worker/ftdb/data/'+ str(departement)
+    
+    # get the list path of files to process
+    path_list = get_path_lst(dirpath)
+    if(path_list == None or len(path_list) <= 0):
         return
     
     #connect to the database
@@ -37,7 +63,7 @@ def main():
     db.print_table()
     
 
-    print("Start of reading" ,len(files_list), "files\n", files_list)
+    print("Start of processing" ,len(path_list), "files\n", path_list)
     # count = 0
     # #build the path and get the object json and process the object
     # for file in files_list:
@@ -54,4 +80,4 @@ def main():
                 
     #         count += 1
 
-main()
+main(74)
