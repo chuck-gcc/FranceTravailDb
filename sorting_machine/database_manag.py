444d62b21d6ebe55f534cecc89b11d729a2124b1
chuck-gcc <3wll@proton.me>
2025-08-17 22:37:48 -0400

    default_push

---
+++ sorting_machine/database_manag.py
@@ -0,0 +1,42 @@
+
+import sqlite3
+
+def connect_to_db():
+    try:
+        con = sqlite3.connect("annonces.db")
+        return(con)
+    except:
+        print("Error connection db")
+
+
+
+class DB:
+
+    def __init__(self):
+        self.db = connect_to_db()
+        self.cursor = self.db.cursor()
+        self.cursor.execute(
+        """CREATE TABLE IF NOT EXISTS annonces_hash ( 
+            id INTEGER PRIMARY KEY,
+            hash BLOB
+        );""")
+
+    def print_table(self):
+        print("Start lecture")
+        self.cursor.execute("""SELECT name FROM sqlite_master WHERE type= 'table'""")
+        data = self.cursor.fetchall()
+        if(len(data) > 0):
+            print("table:")
+            for table in data:
+                print("\t", table[0])
+
+    def create_random_table(self):
+        self.cursor.execute(
+        """CREATE TABLE IF NOT EXISTS random ( 
+            id INTEGER PRIMARY KEY,
+            i   INTEGER
+        );""")
+    
+    def delete_table(self, name):
+        self.cursor.execute(
+        """DROP TABLE VALUE(?);""", name)
--- sorting_machine/sorting_machine.py
+++ sorting_machine/sorting_machine.py
@@ -1,9 +1,9 @@
 import json
 import hashlib
 import pandas as pd
-import sqlite3
 import os
 import base64
+import database_manag
 
 def get_files_lst(dirpath):
     try:
@@ -21,23 +21,37 @@ def get_obj_json(filepath):
             print("Loading object error")
             return(None)
 
+
+
 def main():
 
     #get file list to check
-    dirpath = "/home/cc/Documents/data_worker/ftdb/data/74/2025-08-17T12:40:07.614Z"
+    dirpath = "/home/cc/Documents/data_worker/ftdb/data/74/2025-08-18T00:29:06.296Z"
     files_list = get_files_lst(dirpath)
     if(files_list == None or len(files_list) <= 0):
         return
     
+    #connect to the database
+    db = database_manag.DB()
+    db.create_random_table()
+    db.print_table()
+    
+
     print("Start of reading" ,len(files_list), "files\n", files_list)
-    count = 0
-    #build the path and get the object json and process the object
-    for file in files_list:
-        filepath = dirpath + "/" +file
-        objs = get_obj_json(filepath)
-        if(objs != None):
-            for obj in objs:
-                print(obj['id'])
-                count += 1
-    print("voici le compte: " + count)
+    # count = 0
+    # #build the path and get the object json and process the object
+    # for file in files_list:
+    #     filepath = dirpath + "/" +file
+    #     objs = get_obj_json(filepath)
+    #     if(objs != None):
+    #         json_str = json.dumps(objs[0])
+    #         h = json_str.encode("utf-8")
+    #         print(type(h))
+    #         d = h.decode("utf-8")
+    #         print(d[0])
+    #         # for i in h:
+    #         #     print("voici le compte: " , i)  
+                
+    #         count += 1
+
 main()
