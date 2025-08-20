3c971151cf7990c4f32ff0766a7b52761cbdc4f8
chuck-gcc <3wll@proton.me>
2025-08-20 09:53:39 -0400

    default_push

--- scraper_script.sh
+++ scraper_script.sh
@@ -2,12 +2,4 @@
 
 export PATH="$HOME/bin:$PATH"
 
-# Debug
-# which node
-# which npm
-# node -v
-# npm -v
-
-# pwd
-
 cd /home/cc/Documents/france_travail_worker/job_scrapper && npm run dev
--- sorting_script.sh
+++ sorting_script.sh
@@ -2,12 +2,4 @@
 
 export PATH="$HOME/bin:$PATH"
 
-# Debug
-# which node
-# which npm
-# node -v
-# npm -v
-
-# pwd
-
 cd /home/cc/Documents/france_travail_worker/sorting_machine && python3 sorting_machine.py
